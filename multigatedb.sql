05de0b9ba45417017ecf692ee15963e9740892d8
Robbert Müller <spam.me@grols.ch>
2012-04-21 14:51:50 +0200

    fresh sql dump of a clean table
    mysql didn't like the old dump anymore

--- multigatedb.sql
+++ multigatedb.sql
@@ -1,89 +1,201 @@
--- MySQL dump 8.21
+-- MySQL dump 10.13  Distrib 5.1.61, for debian-linux-gnu (i486)
 --
--- Host: localhost    Database: multigate
----------------------------------------------------------
--- Server version	3.23.49-log
+-- Host: localhost    Database: mjrmultidev
+-- ------------------------------------------------------
+-- Server version	5.1.61-0+squeeze1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table 'address'
+-- Table structure for table `address`
 --
 
-CREATE TABLE address (
-  username varchar(32) NOT NULL default '',
-  protocol varchar(32) NOT NULL default '',
-  address varchar(255) NOT NULL default '',
-  main_address enum('true','false') NOT NULL default 'false',
-  KEY address (address),
-  KEY userprot (username,protocol),
-  KEY user (username)
-) TYPE=MyISAM;
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `address` (
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `protocol` varchar(32) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `main_address` enum('true','false') NOT NULL DEFAULT 'false',
+  KEY `address` (`address`),
+  KEY `userprot` (`username`,`protocol`),
+  KEY `user` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table 'alias'
+-- Dumping data for table `address`
 --
 
-CREATE TABLE alias (
-  alias varchar(32) NOT NULL default '',
-  username varchar(32) NOT NULL default '',
-  PRIMARY KEY  (alias)
-) TYPE=MyISAM;
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table 'box'
+-- Table structure for table `alias`
 --
 
-CREATE TABLE box (
-  boxname varchar(32) NOT NULL default '',
-  username varchar(32) NOT NULL default '',
-  contents int(11) NOT NULL default '0',
-  UNIQUE KEY boxuser (boxname,username)
-) TYPE=MyISAM;
+DROP TABLE IF EXISTS `alias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alias` (
+  `alias` varchar(32) NOT NULL DEFAULT '',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`alias`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table 'herd'
+-- Dumping data for table `alias`
 --
 
-CREATE TABLE herd (
-  herdname varchar(32) NOT NULL default '',
-  username varchar(32) NOT NULL default '',
-  shepherd enum('no','yes') default 'no',
-  protocol varchar(32) default NULL,
-  UNIQUE KEY herduser (herdname,username),
-  KEY herdname (herdname)
-) TYPE=MyISAM;
+LOCK TABLES `alias` WRITE;
+/*!40000 ALTER TABLE `alias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alias` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table 'prefprot'
+-- Table structure for table `box`
 --
 
-CREATE TABLE prefprot (
-  username varchar(32) NOT NULL default '',
-  protocol varchar(32) NOT NULL default '',
-  PRIMARY KEY  (username)
-) TYPE=MyISAM;
+DROP TABLE IF EXISTS `box`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `box` (
+  `boxname` varchar(32) NOT NULL DEFAULT '',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `contents` int(11) NOT NULL DEFAULT '0',
+  UNIQUE KEY `boxuser` (`boxname`,`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table 'protocol'
+-- Dumping data for table `box`
 --
 
-CREATE TABLE protocol (
-  protocol varchar(32) NOT NULL default '',
-  level int(10) unsigned NOT NULL default '0',
-  maxmsgsize int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (protocol)
-) TYPE=MyISAM;
+LOCK TABLES `box` WRITE;
+/*!40000 ALTER TABLE `box` DISABLE KEYS */;
+/*!40000 ALTER TABLE `box` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table 'user'
+-- Table structure for table `herd`
 --
 
-CREATE TABLE user (
-  username varchar(32) NOT NULL default '',
-  irl varchar(64) NOT NULL default '',
-  level int(10) unsigned NOT NULL default '0',
-  password varchar(32) NOT NULL default '',
-  birthday date default NULL,
-  birthtime time default NULL,
-  PRIMARY KEY  (username)
-) TYPE=MyISAM;
+DROP TABLE IF EXISTS `herd`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `herd` (
+  `herdname` varchar(32) NOT NULL DEFAULT '',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `shepherd` enum('no','yes') DEFAULT 'no',
+  `protocol` varchar(32) DEFAULT NULL,
+  UNIQUE KEY `herduser` (`herdname`,`username`),
+  KEY `herdname` (`herdname`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `herd`
+--
+
+LOCK TABLES `herd` WRITE;
+/*!40000 ALTER TABLE `herd` DISABLE KEYS */;
+/*!40000 ALTER TABLE `herd` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prefprot`
+--
+
+DROP TABLE IF EXISTS `prefprot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prefprot` (
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `protocol` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prefprot`
+--
+
+LOCK TABLES `prefprot` WRITE;
+/*!40000 ALTER TABLE `prefprot` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prefprot` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `protocol`
+--
+
+DROP TABLE IF EXISTS `protocol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `protocol` (
+  `protocol` varchar(32) NOT NULL DEFAULT '',
+  `level` int(10) unsigned NOT NULL DEFAULT '0',
+  `maxmsgsize` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`protocol`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `protocol`
+--
+
+LOCK TABLES `protocol` WRITE;
+/*!40000 ALTER TABLE `protocol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `protocol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `irl` varchar(64) NOT NULL DEFAULT '',
+  `level` int(10) unsigned NOT NULL DEFAULT '0',
+  `password` varchar(32) NOT NULL DEFAULT '',
+  `birthday` date DEFAULT NULL,
+  `birthtime` time DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-04-21 14:50:20
